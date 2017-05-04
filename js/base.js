985c60c3c9525866fd9e1d2cd1b9a2260f6e6db0
Albert Mejia <albert@imgix.com>
2017-05-04 13:55:08 -0800

    Deployed af5e1ce with MkDocs version: 0.16.3

--- js/base.js
+++ js/base.js
@@ -1,20 +1,19 @@
-
 /* Highlight */
 $( document ).ready(function() {
     hljs.initHighlightingOnLoad();
     $('table').addClass('table table-striped table-hover');
-});
 
+    /* Automatically focus on modal input when searching */
+    $('#mkdocs_search_modal').on('shown.bs.modal', function () {
+        $('#mkdocs-search-query').focus();
+    });
+});
 
 $('body').scrollspy({
     target: '.bs-sidebar',
 });
 
-
 /* Prevent disabled links from causing a page reload */
 $("li.disabled a").click(function() {
     event.preventDefault();
 });
-
-
-
